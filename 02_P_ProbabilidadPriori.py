639a69176fdfb472b555e0feb75f291b9d6119dd
MoisesDelgadillo <moisesdelgadillowebsites@gmail.com>
2024-05-05 22:11:37 -0600

    Probabilidad

---
+++ 02_P_ProbabilidadPriori.py
@@ -0,0 +1,11 @@
+# ERICK MOISES DELGADILLO LARA
+# R: 21310139 - 6E1
+
+# Definimos la cantidad total de caras en el dado
+total_caras = 6
+
+# Calculamos la probabilidad a priori para cada resultado posible
+probabilidad_priori = 1 / total_caras
+
+# Imprimimos la probabilidad a priori
+print("La probabilidad a priori para cada número en el lanzamiento de un dado justo es:", probabilidad_priori)
